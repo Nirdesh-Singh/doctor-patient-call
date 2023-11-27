6bab48caee7e72e6d191a9f36b639e185152366a
Nirdesh <1onlynirdesh@gmail.com>
2023-11-28 00:57:20 +0530

    Favicon

--- server/index.js
+++ server/index.js
@@ -6,6 +6,11 @@ const socket = require('./lib/socket');
 const app = express();
 const server = http.createServer(app);
 
+// Serve the favicon.ico file
+app.get('/favicon.ico', (req, res) => {
+  res.sendFile(`${__dirname}/../client/dist/favicon.ico`);
+});
+
 app.use('/', express.static(`${__dirname}/../client/dist`));
 
 server.listen(config.PORT, () => {
